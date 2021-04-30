ccf68b99303f7d7501d428879970c05e67e667e3
lethaiduy94 <lethaiduy94@gmail.com>
2021-04-30 11:48:06 +0900

    Project's Initial Commit

---
+++ src/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-ons-page>
+    <p style="text-align: center">
+      Hello guys
+      <ul>
+        <li>Code1</li>
+        <li>Code2</li>
+        <li>Code3</li>
+        <li>Code4</li>
+        <li>Code5</li>
+      </ul>
+    </p>
+  </v-ons-page>
+</template>
+<style scoped>
+  ul li {
+    list-style: none;
+    text-transform: uppercase;
+  }
+</style>
